531f9459043ed5de49af3bfeabb22deebff19e09
Chris Wilcoxson <chris@slushman.com>
2018-08-31 15:39:40 -0500

    Add SVGs. Footer complete.

--- footer.php
+++ footer.php
@@ -9,17 +9,22 @@
  * @package GTChurch2018
  */
 
+$mods = get_theme_mods();
+
 	?><footer class="site-footer" id="colophon">
-		<div class="site-credits">
-			<div class="copyright">&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo esc_url( get_admin_url() ); ?>"><?php echo get_bloginfo( 'name' ); ?></a></div>
-		</div><!-- .site-credits -->
-		<div class="site-info">
-			<address><?php echo get_theme_mod( 'company_address' ); ?></address><?php
+		<div class="copyright">&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo esc_url( get_admin_url() ); ?>"><?php echo get_bloginfo( 'name' ); ?></a></div>
+		<address class="footer-address"><?php echo esc_html( $mods['company_address'] ); ?></address>
+		<address class="footer-address">
+			<a href="tel:<?php echo esc_attr( gt_format_phone_number( $mods['company_phone'] ) ); ?>"><?php 
+			
+				echo esc_html( $mods['company_phone'] ); 
+				
+			?></a>
+		</address><?php
 
-			get_template_part( 'template-parts/menu', 'social' );
+		get_template_part( 'template-parts/menu', 'social' );
 
-		?></div><!-- .site-info -->
-	</footer><!-- .site-footer --><?php 
+	?></footer><!-- .site-footer --><?php 
 
 	wp_footer(); 
 
